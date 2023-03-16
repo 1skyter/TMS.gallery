014cae28018b9417d5c02195e23f2c53dfd9387e
1skyter <rodiontrach78@icloud.com>
2023-03-16 21:05:49 +0100

    gallery-1
    prepared project

---
+++ diplom.gallery/diplom.gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diplom.gallery
+//
+//  Created by Rodion Trach on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
